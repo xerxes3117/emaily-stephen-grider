5959a60bb8537c857a24466234c4b66bd3e58b42
xerxes3117 <vsnsit@gmail.com>
2023-06-19 20:50:38 +0530

    emaily oauth section done

--- server/index.js
+++ server/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+const PORT = process.env.PORT || 5002;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome');
-})
+//PASSPORT JS setup
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.GOOGLE_CLIENT_ID,
+    clientSecret: keys.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken)
+    console.log(refreshToken)
+    console.log(profile)
+  })
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT);
